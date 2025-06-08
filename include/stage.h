f08558635e871ec2d6bfc2e0d33f1b05e0bc6456
shilberbullet <lsy021311@gmail.com>
2025-06-09 01:09:33 +0900

    refactor: centralize stage types

--- include/stage.h
+++ include/stage.h
@@ -1,32 +1,7 @@
 #ifndef STAGE_H
 #define STAGE_H
 
-#include <stdbool.h>
-#include "yokai.h"
-
-#define REGION_NAME_MAX 32
-#define MAX_ENEMIES 5
-
-typedef enum {
-    TERRAIN_MOUNTAIN,
-    TERRAIN_RIVER,
-    TERRAIN_SEA,
-    TERRAIN_FIELD,
-    TERRAIN_VILLAGE,
-    TERRAIN_COUNT
-} TerrainType;
-
-typedef struct {
-    int stageNumber;
-    bool isBossStage;
-    int minLevel;
-    int maxLevel;
-    int enemyCount;
-    Yokai enemies[MAX_ENEMIES];
-    char region[REGION_NAME_MAX];
-    TerrainType terrain;
-    int hour;
-} StageInfo;
+#include "stage_types.h"
 
 extern StageInfo currentStage;
 
